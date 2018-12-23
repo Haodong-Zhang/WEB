84c085ae07b81ca5597f7c6d624ac293c9474a9e
Haodong Zhang <u6150801@anu.edu.au>
2018-12-23 23:17:27 +1100

    Creat app

---
+++ forum/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+lmport './pages/landing_page.js'
+
+class App extends Component {
+  render() {
+    return (
+        
+    );
+  }
+}
+
+export default App;
